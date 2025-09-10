4e0052168b8a1e28d43b0b72ac51574ea62c7d4f
VenixBR <viniciusschultz2405@gmail.com>
2025-09-10 18:11:40 -0300

    work directory added to gitignore

--- ACTV_04_SquareRoot_SecondVersion/Synthesis/work/xcelium.d/run.lnx86.23.03.d/.timestamp.ts
+++
@@ -1,9 +0,0 @@
-1757535135 /home/schultz/Documents/ARQIII_Works/ACTV_04_SquareRoot_SecondVersion/HDL/Described_HDLs/Stage2.v
-1757537999 /home/schultz/Documents/ARQIII_Works/ACTV_04_SquareRoot_SecondVersion/HDL/Described_HDLs/DataPath.v
-1757535106 /home/schultz/Documents/ARQIII_Works/ACTV_04_SquareRoot_SecondVersion/HDL/Described_HDLs/Comparator.v
-1757535130 /home/schultz/Documents/ARQIII_Works/ACTV_04_SquareRoot_SecondVersion/HDL/Described_HDLs/Stage1.v
-1757535490 /home/schultz/Documents/ARQIII_Works/ACTV_04_SquareRoot_SecondVersion/Testbenchs/Square_root_tb.sv
-1757535102 /home/schultz/Documents/ARQIII_Works/ACTV_04_SquareRoot_SecondVersion/HDL/Described_HDLs/CLA.v
-1757535120 /home/schultz/Documents/ARQIII_Works/ACTV_04_SquareRoot_SecondVersion/HDL/Described_HDLs/RegSquare2.v
-1757535126 /home/schultz/Documents/ARQIII_Works/ACTV_04_SquareRoot_SecondVersion/HDL/Described_HDLs/Square_root.v
-1757535703 /home/schultz/Documents/ARQIII_Works/ACTV_04_SquareRoot_SecondVersion/HDL/Described_HDLs/ControlPath.v
